1974eb9d9f9bcb587e1cc6d3af2b093a0ffa60f1
Bruno <bruuh.cor@gmail.com>
2019-08-19 20:10:13 -0300

    Adicionado banner com animações simples

---
+++ scripts/slider.js
@@ -0,0 +1,44 @@
+// var slides = document.querySelectorAll(".slide");
+// var buttons = document.querySelectorAll(".banner-button");
+// var banner = document.querySelector(".banner");
+
+// function loadSlides() {
+//     for (let i = 0; i < slides.length; i++) {
+//         const e = slides[i];
+//         s = i + 1;
+//         e.style.backgroundImage = "url('../images/banner-example-" + s + ".jpg')";
+//     }
+// }
+// loadSlides();
+
+// currentSlide(1);
+
+// function currentSlide(s) {
+//     n = (parseInt(s) - 1);
+//     for (let i = 0; i < buttons.length; i++) {
+//         slides[i].style.display = "none";
+//         buttons[i].style.backgroundColor = "gray";
+//     }
+//     slides[n].style.display = "inherit";
+//     buttons[n].style.backgroundColor = "#ff2020";
+// }
+
+var banner = document.querySelector(".banner-test");
+var bannerTexts = document.querySelectorAll(".banner-text");
+var buttons = document.querySelectorAll(".banner-button");
+
+currentSlide(1);
+
+function currentSlide(s) {
+    n = (parseInt(s) - 1);
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "gray";
+        bannerTexts[i].style.display = "none";
+        bannerTexts[i].style.opacity = 0;
+    }
+    banner.style.backgroundImage = "url('../images/banner-example-" + s + ".jpg')";
+    bannerTexts[n].style.display = "inherit";
+    bannerTexts[n].style.opacity = 1;
+
+    buttons[n].style.backgroundColor = "#ff2020";
+}
