7136b18e3eabfe8d821a0f2bafae6bcafe81a495
Bruno <bruuh.cor@gmail.com>
2019-09-06 00:53:45 -0300

    Versão 1.4.1

--- scripts/setor-responsivo.js
+++ scripts/setor-responsivo.js
@@ -1,32 +1,51 @@
-// document.ondragstart = function () { return false; };
-// const img = document.querySelector("img");
-// img.ondragstart = function () { return false; }
-
-/* IE11 Fix for SP2010 */
+document.ondragstart = function() {
+  return false;
+};
+const img = document.querySelectorAll("img");
+img.ondragstart = function() {
+  return false;
+};
 
 if (typeof UserAgentInfo != "undefined" && !window.addEventListener) {
   UserAgentInfo.strBrowser = 1;
 }
 
-const slider = document.querySelector(".setores");
+if(/MSIE \d | Trident.*rv:/.test(navigator.userAgent)) {
+  var scrollEnd = 0.67;
+}
 
+else scrollEnd = 0.70;
+
+const slider = document.querySelector(".setores");
+const arrows = document.querySelectorAll(".setores-ui .arrow");
+const arrowL = document.querySelector(".setores-ui .arrow:nth-child(1)");
+const arrowR = document.querySelector(".setores-ui .arrow:nth-child(3)");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', function(e) {
+//Start
+window.setTimeout(function(){
+  slider.scrollLeft = 0;
+}, 250);
+changeArrowsDisplay();
+
+slider.addEventListener("mousedown", function(e) {
   isDown = true;
   slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
+  changeArrowsDisplay();
 });
 slider.addEventListener("mouseleave", function() {
   isDown = false;
   slider.classList.remove("active");
+  changeArrowsDisplay();
 });
 slider.addEventListener("mouseup", function() {
   isDown = false;
   slider.classList.remove("active");
+  changeArrowsDisplay();
 });
 slider.addEventListener("mousemove", function(e) {
   if (!isDown) return;
@@ -35,3 +54,17 @@ slider.addEventListener("mousemove", function(e) {
   const walk = (x - startX) * 1.5;
   slider.scrollLeft = scrollLeft - walk;
 });
+
+function changeArrowsDisplay() {
+  for (let i = 0; i < arrows.length; i++) {
+    arrows[i].style.opacity = 0.7;
+  }
+
+  if (slider.scrollLeft >= slider.scrollWidth * scrollEnd) { //1070
+    arrowR.style.opacity = 0;
+  } else if (slider.scrollLeft <= slider.scrollWidth * 0.02) { //20
+    arrowL.style.opacity = 0;
+  }
+}
+
+console.log(slider.scrollLeft);
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,54 +1,55 @@
-// var slides = document.querySelectorAll(".slide");
-// var buttons = document.querySelectorAll(".banner-button");
-// var banner = document.querySelector(".banner");
-
-// function loadSlides() {
-//     for (let i = 0; i < slides.length; i++) {
-//         const e = slides[i];
-//         s = i + 1;
-//         e.style.backgroundImage = "url('../images/banner-example-" + s + ".jpg')";
-//     }
-// }
-// loadSlides();
-
-// currentSlide(1);
-
-// function currentSlide(s) {
-//     n = (parseInt(s) - 1);
-//     for (let i = 0; i < buttons.length; i++) {
-//         slides[i].style.display = "none";
-//         buttons[i].style.backgroundColor = "gray";
-//     }
-//     slides[n].style.display = "inherit";
-//     buttons[n].style.backgroundColor = "#ff2020";
-// }
+var images = new Array();
+for (let i = 0; i < images.length; i++) {
+    const element = images[i];
+    element = new Image();
+    element.src = "('resources/images/banner/banner-example-" + (i + 1) + ".jpg')";
+}
 
 var banner = document.querySelector(".banner");
 var bannerTexts = document.querySelectorAll(".banner-text");
 var buttons = document.querySelectorAll(".banner-button");
 
-var canChange = true;;
+var currentSlide;
+var canChange = true;
+var interval;
 
-currentSlide(1);
+changeSlide(1);
 
-function currentSlide(s) {
+// function changeTimer(time) {
+//     window.clearTimeout();
+//     window.setTimeout(function() {
+//         if(!canChange) return;
+//         currentSlide++;
+//         if(currentSlide > 3) currentSlide = 1;
+//         changeSlide(currentSlide);
+//     }, time);    
+// }
+
+function changeSlide(s) {
     if(!canChange) return;
     canChange = false;
     
+    currentSlide = s;
     n = (parseInt(s) - 1);
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.backgroundColor = "gray";
         bannerTexts[i].style.display = "none";
         bannerTexts[i].style.opacity = 0;
     }
-    banner.style.backgroundImage = "url('resources/images/banner-example-" + s + ".jpg')";
+    banner.style.backgroundImage = "url('resources/images/banner/banner-example-" + s + ".jpg')";
     bannerTexts[n].style.display = "inherit";
     bannerTexts[n].style.opacity = 1;
 
     buttons[n].style.backgroundColor = "#ff2020";
-
-    window.setTimeout(function(){
-        canChange = true;   
-    }, 1000);
-
+    changeDelay();
 }
+
+function changeDelay() {
+    // slideTimer(7000);
+    window.setTimeout(function(){
+        canChange = true;
+
+    }, 1000);
+}
+
+//<body onload= "função()"> no html pode ser útil
